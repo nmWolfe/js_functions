b814d0445010f40e761c38c152bcb07e9c8fe3d2
Nick <n.m.phelan@gmail.com>
2023-07-09 10:33:09 +0100

    Added more functions

--- functions-2.js
+++ functions-2.js
@@ -9,3 +9,26 @@ const getDescription = (jobTitle, location, partners_name, amountOfChildren) => 
 }
 
 getDescription('Developer', 'Portugal', 'Layla', 6)
+
+// **The Lifetime Supply of Snickers**
+
+// Ever wonder how much a lifetime supply of Snickers would actually cost? Wonder no more!
+
+// Write a function named calculateSupply that:
+    // Takes 2 arguments: age, amountPerDay.
+    // Calculated the amount conumed for the rest of your life (based on a constant max age).
+    // Outputs the result to the console like so: "You will need X to last you until the ripe old age of Y"
+    // Call that function three times, passing in different values each time.
+    // BONUS: Accept floating point values for amount per day, and round the result to a round number.
+
+const calculateSupply = (currentAge, amountPerDay) => {
+
+    const maxAge = 99;
+    const amountOfSnickers = (maxAge - currentAge) * (amountPerDay * 365);
+    console.log(`You will need ${amountOfSnickers} Snickers, to last you to the ripe old age of ${maxAge}.`);
+
+}
+
+calculateSupply(32, 4);
+calculateSupply(28, 4);
+calculateSupply(20, 2);
